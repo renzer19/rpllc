88645c0e2bfbae2c04fcaa0e3066aaf68c0da65a
renaldi <87884489+renzer19@users.noreply.github.com>
2022-04-17 14:14:55 +0700

    Add files via upload

---
+++ 4januari1/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host       = "localhost";
+    $user       = "root";
+    $password   = "";
+    $dbname     = "webkelas";
+
+    $sambung=mysqli_connect($host,$user,$password,$dbname);
+?>
